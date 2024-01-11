a11842d829595c38e66b6135fe2260d4f5e86122
Yosuke Kobayashi <yosuke.kobayashi@ipmu.jp>
2024-01-11 11:43:52 -0700

    removed MasterIntegral from utils codes.

--- src/powercovfft/cov.py
+++ src/powercovfft/cov.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy.integrate import quad
 
 from .power_law_decomp import PowerLawDecomp
+from .master_integral import MasterIntegral
 from . import utils
 
 
@@ -9,7 +10,7 @@ class PowerSpecCovFFT:
 
     def __init__(self):
         print('Loading master integrals...')
-        self.get_master_int = utils.MasterIntegral()
+        self.get_master_int = MasterIntegral()
 
         print('Loading coefficient functions...')
         self.set_coeff_func()
@@ -58,9 +59,9 @@ class PowerSpecCovFFT:
         k1 = np.atleast_1d(k1)
         k2 = np.atleast_1d(k2)
 
-        eta_m = np.transpose(np.tile(self.decomp.eta_m, (len(k1),len(k2),1)), (2,0,1))
-        k1_tile = np.tile(k1, (len(eta_m),1,1))
-        k2_tile = np.tile(k2, (len(eta_m),1,1))
+        eta_m = np.transpose(np.tile(self.decomp.eta_m, (len(k1), len(k2), 1)), (2, 0, 1))
+        k1_tile = np.tile(k1, (len(eta_m), 1, 1))
+        k2_tile = np.tile(k2, (len(eta_m), 1, 1))
 
         self.master_int = {}
         for (a,b) in sorted(self.set_ab):
@@ -76,22 +77,33 @@ class PowerSpecCovFFT:
         k1 = np.atleast_1d(k1)
         k2 = np.atleast_1d(k2)
 
-        terms = []
-        for (a,b) in sorted(self.set_ab):
-            if not (l1,l2,a,b) in self.coeff_func[name].expr.keys(): continue
-            coeff = self.coeff_func[name](a, b, l1, l2, k1, k2, self.fgrowth, self.bias)
-            term = coeff * self.base_int[(a,b)]
-            terms.append(term)
-        res = np.sum(terms, axis=0)
+        if name in self.coeff_func.keys():
+            terms = []
+            for (a,b) in sorted(self.set_ab):
+                if not (l1,l2,a,b) in self.coeff_func[name].expr.keys(): continue
+                coeff = self.coeff_func[name](a, b, l1, l2, k1, k2, self.fgrowth, self.bias)
+                term = coeff * self.base_int[(a,b)]
+                terms.append(term)
+            res = np.sum(terms, axis=0)
+        
+        elif name in self.cov_integral.keys():
+            res = self.cov_integral[name](l1, l2, k1, k2, self.fgrowth, self.bias)
+
+        else:
+            raise ValueError('The term name %s is invalid.' % (name))
 
         if len(k1) == 1 or len(k2) == 1:
             res = np.ravel(res)
         if len(k1) == 1 and len(k2) == 1:
             res = res[0]
         return res
+    
+    def get_cov_T0_integrand(self, mu12, l1, l2, k1, k2, name):
+        res = self.cov_integrand[name](mu12, l1, l2, k1, k2, self.fgrowth, self.bias)
+        return res
 
     def get_cov_T0_term_direct(self, l1, l2, k1, k2, name, epsrel=1e-8, limit=10000):
-        integrand = lambda mu: self.cov_integrand[name](mu, l1, l2, k1, k2, self.fgrowth, self.bias) * self.get_pk_lin(np.sqrt(k1**2 + k2**2 + 2 * k1 * k2 * mu))
+        integrand = lambda mu: self.get_cov_T0_integrand(mu, l1, l2, k1, k2, name) * self.get_pk_lin(np.sqrt(k1**2 + k2**2 + 2 * k1 * k2 * mu))
         res = quad(integrand, -1, 1, epsrel=epsrel, limit=limit)[0] / 2
         return res
 
@@ -104,12 +116,12 @@ class PowerSpecCovFFT:
 
     def get_cov_T3111(self, l1, l2, k1, k2):
         # T0 contribution from T3111 ("star") term, whose integration is done analytically.
-        term1 = 12 * self.get_pk_lin(k1)**2 * self.get_pk_lin(k2) * self.cov_integral['T3111'](l1, l2, k1, k2, self.fgrowth, self.bias)
-        term2 = 12 * self.get_pk_lin(k2)**2 * self.get_pk_lin(k1) * self.cov_integral['T3111'](l1, l2, k2, k1, self.fgrowth, self.bias)
+        term1 = 12 * self.get_pk_lin(k1)**2 * self.get_pk_lin(k2) * self.get_cov_T0_term(l1, l2, k1, k2, name='T3111')
+        term2 = 12 * self.get_pk_lin(k2)**2 * self.get_pk_lin(k1) * self.get_cov_T0_term(l1, l2, k2, k1, name='T3111')
         return term1 + term2
 
     def get_cov_T_SN_B(self, l1, l2, k1, k2):
-        term1 = 8 * self.get_pk_lin(k1) * self.get_pk_lin(k2) * self.cov_integral['T_SN_B_1'](l1, l2, k1, k2, self.fgrowth, self.bias)
+        term1 = 8 * self.get_pk_lin(k1) * self.get_pk_lin(k2) * self.get_cov_T0_term(l1, l2, k1, k2, name='T_SN_B_1')
         term2 = 8 * self.get_pk_lin(k1) * self.get_cov_T0_term(l1, l2, k1, k2, name='T_SN_B_2')
         term3 = 8 * self.get_pk_lin(k2) * self.get_cov_T0_term(l1, l2, k2, k1, name='T_SN_B_2')
         return (term1 + term2 + term3) / self.ndens
@@ -139,16 +151,16 @@ class PowerSpecCovFFT:
         return cov
 
     def get_cov_T0_SN_direct(self, l1, l2, k1, k2, epsrel=1e-8, limit=10000):
-        term_SN_B1 = 8 / self.ndens * self.get_pk_lin(k1) * self.get_pk_lin(k2) * self.cov_integral['T_SN_B_1'](l1, l2, k1, k2, self.fgrowth, self.bias)
+        term_SN_B1 = 8 / self.ndens * self.get_pk_lin(k1) * self.get_pk_lin(k2) * self.get_cov_T0_term(l1, l2, k1, k2, name='T_SN_B_1')
         term_SN_B2_P = quad(self.get_T0_SN_integrand, -1, 1, args=(l1,l2,k1,k2), epsrel=epsrel, limit=limit)[0] / 2
         cov = (term_SN_B1 + term_SN_B2_P) / self.vol
         return cov
 
     def get_T2211_integrand(self, mu12, l1, l2, k1, k2):
         # T0 integrand from T2211 ("snake") terms
-        term1 = 8 * self.get_pk_lin(k1)**2 * self.cov_integrand['T2211_1'](mu12, l1, l2, k1, k2, self.fgrowth, self.bias)
-        term2 = 8 * self.get_pk_lin(k2)**2 * self.cov_integrand['T2211_1'](mu12, l1, l2, k2, k1, self.fgrowth, self.bias)
-        term3 = 16 * self.get_pk_lin(k1) * self.get_pk_lin(k2) * self.cov_integrand['T2211_2'](mu12, l1, l2, k1, k2, self.fgrowth, self.bias)
+        term1 = 8 * self.get_pk_lin(k1)**2 * self.get_cov_T0_integrand(mu12, l1, l2, k1, k2, name='T2211_1')
+        term2 = 8 * self.get_pk_lin(k2)**2 * self.get_cov_T0_integrand(mu12, l1, l2, k2, k1, name='T2211_1')
+        term3 = 16 * self.get_pk_lin(k1) * self.get_pk_lin(k2) * self.get_cov_T0_integrand(mu12, l1, l2, k1, k2, name='T2211_2')
 
         k12 = np.sqrt(k1**2 + k2**2 + 2 * k1 * k2 * mu12)
         integrand = self.get_pk_lin(k12) * (term1 + term2 + term3)
@@ -156,9 +168,9 @@ class PowerSpecCovFFT:
 
     def get_T0_SN_integrand(self, mu12, l1, l2, k1, k2):
         # T0 integrand from shot-noise terms
-        term1 = 8 / self.ndens * self.get_pk_lin(k1) * self.cov_integrand['T_SN_B_2'](mu12, l1, l2, k1, k2, self.fgrowth, self.bias)
-        term2 = 8 / self.ndens * self.get_pk_lin(k2) * self.cov_integrand['T_SN_B_2'](mu12, l1, l2, k2, k1, self.fgrowth, self.bias)
-        term3 = 2 / self.ndens**2 * self.cov_integrand['T_SN_P'](mu12, l1, l2, k1, k2, self.fgrowth, self.bias)
+        term1 = 8 / self.ndens * self.get_pk_lin(k1) * self.get_cov_T0_integrand(mu12, l1, l2, k1, k2, name='T_SN_B_2')
+        term2 = 8 / self.ndens * self.get_pk_lin(k2) * self.get_cov_T0_integrand(mu12, l1, l2, k2, k1, name='T_SN_B_2')
+        term3 = 2 / self.ndens**2 * self.get_cov_T0_integrand(mu12, l1, l2, k2, k1, name='T_SN_P')
 
         k12 = np.sqrt(k1**2 + k2**2 + 2 * k1 * k2 * mu12)
         integrand = self.get_pk_lin(k12) * (term1 + term2 + term3)
--- src/powercovfft/utils_mt.py
+++ src/powercovfft/utils_mt.py
@@ -1,65 +1,9 @@
 import os
 import glob, re
 import numpy as np
-from scipy.special import loggamma
 from sympy.parsing.mathematica import parse_mathematica
 from sympy import var, lambdify
 
-
-class MasterIntegral:
-
-    def __init__(self):
-        dirname = os.path.dirname(__file__)
-        
-        fnames = [dirname+'/master_int/poly_b%s.txt' % (b) for b in range(13)]
-        self.expr_poly = []
-        for fname in fnames:
-            with open(fname,'r') as file:
-                expr = file.read()
-            expr = parse_mathematica(expr)
-            self.expr_poly.append(expr)
-            
-        fnames = [dirname+'/master_int/z0lim20_b%s.txt' % (b) for b in range(13)]
-        self.expr_z0lim = []
-        for fname in fnames:
-            with open(fname,'r') as file:
-                expr = file.read()
-            expr = parse_mathematica(expr)
-            self.expr_z0lim.append(expr)
-            
-        fnames = [dirname+'/master_int/z1_b%s.txt' % (b) for b in range(13)]
-        self.expr_z1 = []
-        for fname in fnames:
-            with open(fname,'r') as file:
-                expr = file.read()
-            expr = parse_mathematica(expr)
-            self.expr_z1.append(expr)
-        
-        z = var('z')
-        a = var('a')
-        
-        self.poly = [lambdify([z,a], expr, modules='numpy') for expr in self.expr_poly]
-        self.z0lim = [lambdify([z,a], expr, modules='numpy') for expr in self.expr_z0lim]
-        self.z1 = [lambdify([a], expr, modules='numpy') for expr in self.expr_z1]
-
-    def __call__(self, a, b, k1, k2, z_switch=0.1):
-        a = np.atleast_1d(a)
-        k1 = np.atleast_1d(k1)
-        k2 = np.atleast_1d(k2)
-
-        z = 2 * k1 * k2 / (k1**2 + k2**2)
-
-        fac = np.exp(loggamma(b+2) - (b+1) * np.log(z) - np.log(np.prod([a-n for n in range(1,b+2)], axis=0)))
-        res = fac * (self.poly[b](-z,a) / (1+z)**a - self.poly[b](z,a) / (1-np.where(z!=1.,z,0))**a)
-        
-        res_z1 = self.z1[b](a * np.ones_like(z))
-        res = res * np.where(z!=1.,1,0) + res_z1 * np.where(z==1.,1,0)
-        
-        res_z0 = self.z0lim[b](z,a)
-        res = res * np.where(np.abs(z)>z_switch,1,0) + res_z0 * np.where(np.abs(z)<=z_switch,1,0)
-        
-        res = (k1**2 + k2**2)**(-a) * res / (2 * (b+1))
-        return res
 
 class CovCoeff:
 
